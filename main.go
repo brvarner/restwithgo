3b081ddba6b4434070c524f9ed854f830f4a3652
brvarner <bvarner4592@gmail.com>
2025-06-03 06:27:26 -0500

    added godotenv package, updated docker-compose to use API language

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"user-management-api/models"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 type User struct {
@@ -19,6 +20,10 @@ type User struct {
 }
 
 func main() {
+	err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
 
 	database.InitDB()
 
