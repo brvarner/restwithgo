2bdeccc506ca4e053fd96ac85984f54e83d5bec3
brvarner <bvarner4592@gmail.com>
2025-06-03 06:45:22 -0500

    app running, switching from .env to secrets

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"user-management-api/models"
 
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
 )
 
 type User struct {
@@ -20,11 +19,7 @@ type User struct {
 }
 
 func main() {
-	err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
-
+	
 	database.InitDB()
 
 	r:= mux.NewRouter()
